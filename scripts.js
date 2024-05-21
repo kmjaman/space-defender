f24fe9806a21f83cb7bcb4658519d28c8f18eb59
Kamrujjaman Md <jaman.edux@gmail.com>
2024-05-21 19:16:33 +0200

    Create game variables

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+// Game variables
+const canvas = document.getElementById('game-canvas');
+const ctx = canvas.getContext('2d');
+const startScreen = document.getElementById('start-screen');
+const gameOverScreen = document.getElementById('game-over');
+const levelNotification = document.getElementById('level-notification');
+const startButton = document.getElementById('start-button');
+const restartButton = document.getElementById('restart-button');
 
+let gameRunning = false;
+let gameOver = false;
+let score = 0;
+let level = 1;
+let lives = 3;
+let powerUpType = "None";
+let powerUpTime = 0;
