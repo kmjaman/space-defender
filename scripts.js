c6056e6880bc1de19211cd35bc373e2a325d0cb8
Kamrujjaman Md <jaman.edux@gmail.com>
2025-02-27 21:00:47 +0100

    Create delete collision function

--- scripts.js
+++ scripts.js
@@ -377,13 +377,20 @@ function checkCollisions() {
             break;
         }
     }
-    
+
     // Check if all enemies are defeated
     if (enemies.length === 0) {
         level++;
         showLevelNotification();
         spawnEnemies();
     }
+}
+
+function detectCollision(obj1, obj2) {
+    return obj1.x < obj2.x + obj2.width &&
+           obj1.x + obj1.width > obj2.x &&
+           obj1.y < obj2.y + obj2.height &&
+           obj1.y + obj1.height > obj2.y;
 }
 
 function createParticles(x, y, count, color) {
